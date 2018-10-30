0b21c3106466bec858206acba6fcc745c53aebc7
lrvine <tsungyeh@gmail.com>
2018-10-31 01:59:46 +0800

    Use auto format code and removed deprecated flag in Makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=gcc
 #CFLAGS= -c -O -march=native -Wall
-CFLAGS= -c -O3 -march=broadwell -mavx2 -fslp-vectorize-aggressive -Wall
-#CFLAGS= -c -O3 -march=broadwell -mavx2 -fslp-vectorize-aggressive -Rpass=loop-vectorize -Rpass-analysis=loop-vectorize -Wall
+#CFLAGS= -c -O3 -march=broadwell -mavx2  -Wall
+CFLAGS= -c -O3 -march=broadwell -mavx2  -Rpass=loop-vectorize -Rpass-analysis=loop-vectorize -Wall
 SOURCE=strassen.c verify.c 
 LDFLAGS=
 OBJECTS= $(SOURCE:.c=.o)
--- strassen.h
+++ strassen.h
@@ -1,11 +1,13 @@
 #ifndef STRASSEN_H
 #define STRASSEN_H
 
-void inline sequentialMatrixMult(int n, double ** A, double ** B, double ** C);
-void inline matrixAdd(int n, double ** A, double ** B, double ** C);
-void inline matrixSub(int n, double ** A, double ** B, double ** C);
-void inline get_sub_matrix( double ** matrix, double ** subMatrix, int row_size,int column_size, int row_offset, int column_offset );
-void four_blocks_to_matrix( int N, int padding, double ** matrix, double ** C11, double ** C12, double ** C21, double ** C22 );
+void inline sequentialMatrixMult(int n, double** A, double** B, double** C);
+void inline matrixAdd(int n, double** A, double** B, double** C);
+void inline matrixSub(int n, double** A, double** B, double** C);
+void inline get_sub_matrix(double** matrix, double** subMatrix, int row_size,
+                           int column_size, int row_offset, int column_offset);
+void four_blocks_to_matrix(int N, int padding, double** matrix, double** C11,
+                           double** C12, double** C21, double** C22);
 void Strassen(int n, double** A, double** B, double** C);
 void fastMatrixMultiplication(int n, double** A, double** B, double** C);
 
